2f6f14f55a3ef45481fddfa147481e7eb12e3357
BRAINGAIN <braingain@braingain-System-Product-Name.(none)>
2011-06-07 18:05:00 +0800

    blog app update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,17 @@
 RailsExercise::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  config.action_mailer.default_url_options = {
+    :host => "localhost:3000"
+  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address => 'smtp.gmail.com',
+      :port => '587',
+      :authentication => :plain,
+      :domain => 'mail@google.com',
+      :user_name => 'fxdgreat@gmail.com',
+      :password => 'iloveeria143'
+  }
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the webserver when you make code changes.
@@ -12,10 +23,10 @@ RailsExercise::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_view.debug_rjs             = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
--- db/schema.rb
+++ db/schema.rb
@@ -65,22 +65,4 @@ ActiveRecord::Schema.define(:version => 20110601074116) do
     t.datetime "updated_at"
   end
 
-  create_table "models", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "models", ["email"], :name => "index_models_on_email", :unique => true
-  add_index "models", ["reset_password_token"], :name => "index_models_on_reset_password_token", :unique => true
-
 end
