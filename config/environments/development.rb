3f4dd0efc4b698e20f9a2461fffb85ba197ae819
BRAINGAIN <braingain@braingain-System-Product-Name.(none)>
2011-06-08 09:26:48 +0800

    Another update for my blog app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 RailsExercise::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   config.action_mailer.default_url_options = {
-    :host => "blogme"
+    :host => "blogme.heroku.com"
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
